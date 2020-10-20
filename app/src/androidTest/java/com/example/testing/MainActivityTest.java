c5f88774bd10c1b750777db18d46e409a4632b07
Robbe Elsermans <robels@outlook.com>
2020-10-20 16:23:36 +0200

    test

---
+++ app/src/androidTest/java/com/example/testing/MainActivityTest.java
@@ -0,0 +1,25 @@
+package com.example.testing;
+
+
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.pressBack;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest() {
+        pressBack();
+    }
+}
